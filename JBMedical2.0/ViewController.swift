feffa2fdae8bee660404423779e4392f8630e778
James Paulk <James@Jamess-MacBook-Pro.local>
2017-04-27 13:08:57 -0500

    Initial Commit

---
+++ JBMedical2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JBMedical2.0
+//
+//  Created by James Paulk on 4/27/17.
+//  Copyright © 2017 James Paulk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
