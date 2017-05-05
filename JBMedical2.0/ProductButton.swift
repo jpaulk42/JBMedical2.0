88fb24ca503466a920a7a89517fa6e8bf09bf09e
James Paulk <James@Jamess-MacBook-Pro.local>
2017-05-05 15:00:06 -0500

    First Commit

---
+++ JBMedical2.0/ProductButton.swift
@@ -0,0 +1,21 @@
+//
+//  ProductButton.swift
+//  JBMedical2.0
+//
+//  Created by James Paulk on 4/28/17.
+//  Copyright © 2017 James Paulk. All rights reserved.
+//
+
+import UIKit
+
+class ProductButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
